b9f296629b453b80de107f310abe357beb7d9e08
hunterdyar <bloop@hdyar.com>
2023-02-27 23:19:41 -0500

    markdown frontmatter initial work

---
+++ Assets/Obsidian Canvas/Scripts/Editor/MarkdownImporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using ObsidianCanvas.MarkdownData;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+[ScriptedImporter(1, new[]{"markdown"}, new []{"md"})]
+public class MarkdownImporter : ScriptedImporter
+{
+	public override void OnImportAsset(AssetImportContext ctx)
+	{
+		var markdownObject = ScriptableObject.CreateInstance<MarkdownObject>();
+		markdownObject.name = Path.GetFileNameWithoutExtension(ctx.assetPath) + " obj";
+
+		markdownObject.SetText(File.ReadAllText(ctx.assetPath));
+		
+		EditorUtility.SetDirty(markdownObject);
+		ctx.AddObjectToAsset("Canvas Object", markdownObject);
+		ctx.SetMainObject(markdownObject);
+	}
+
+
+}
---
+++ Assets/Obsidian Canvas/Scripts/MarkdownData/MarkdownObject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ObsidianCanvas.MarkdownData
+{
+	public class MarkdownObject : ScriptableObject
+	{
+		//Currently there is no advantage to using this importer.
+		//but... frontmatter support into objects!
+		
+		[TextArea(10,30)]
+		public string text;
+
+		public void ParseFrontmatter()
+		{
+			//step 1: find frontmatter
+				//split by end-line, search for "---" or ... lines that have at least, but any number of, dashes?
+			//step 2: parse as YAML using https://github.com/aaubry/YamlDotNet
+			//MarkdownObject<T> where T is what we try to parse front-matter into, the rest is markdown.
+			
+			//Step 3: markdown to textMeshPro-compatible rtf using some converter.
+		}
+
+		public void SetText(string text)
+		{
+			this.text = text;
+		}
+	}
+
+	[Serializable]
+	public class FrontmatterProperty
+	{
+		private string id;
+		private object value;
+	}
+}
